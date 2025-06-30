b9a2102098c4b8d7179076dffd0a0907903f3fbe
Amit <ay5097502@gmail.com>
2025-06-30 19:40:06 +0530

    adding new

---
+++ nothing.py
@@ -0,0 +1,10 @@
+
+
+a = ['Geeks', 'for', 'Geeks']
+
+# Creating an enumerate object from the list 'a' 
+b = enumerate(a)
+
+# This retrieves the first index-element pair from 'b'
+nxt_val = next(b)
+print(nxt_val)
